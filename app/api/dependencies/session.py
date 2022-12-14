e91ee3ec25990ebe238e015b259d0797dcd80591
Aeternalis-Ingenium <aeternalis-ingenium@proton.me>
2022-12-14 22:05:42 +0100

    3.. 2.. 1.. Let the party begin 🚀🚀

---
+++ app/api/dependencies/session.py
@@ -0,0 +1,16 @@
+import contextlib
+import typing
+
+from sqlalchemy.ext.asyncio import AsyncSession as SQLAlchemyAsyncSession
+
+from app.repository.database import async_db
+
+
+async def get_async_session() -> typing.AsyncGenerator[SQLAlchemyAsyncSession, None]:
+    try:
+        yield async_db.async_session
+    except Exception as e:
+        print(e)
+        await async_db.async_session.rollback()
+    finally:
+        await async_db.async_session.close()
